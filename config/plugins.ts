f3726f119b67c830f2a053fb38b77539e1089c5d
raj tayde <rajtayde0607@gmail.com>
2025-05-02 22:24:02 +0530

    update code

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      provider: 'local',  // Using the local file system for uploads
+      providerOptions: {
+        sizeLimit: 10000000, // Set a file size limit (e.g., 10MB)
+      },
+    },
+  });
+  
